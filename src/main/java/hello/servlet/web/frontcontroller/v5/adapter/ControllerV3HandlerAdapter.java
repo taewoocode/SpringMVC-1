d55c09c933eb5ba0e59e7c634e9b7734430d8779
taewoocode <qkrxodn6035@gmail.com>
2024-12-10 23:32:56 +0900

    handler

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller.v5.adapter;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return false;
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return null;
+    }
+}
