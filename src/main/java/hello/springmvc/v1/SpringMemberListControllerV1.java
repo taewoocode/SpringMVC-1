40e9f31685a8c4513be0f3ddc6b3ba1629c7f083
taewoocode <qkrxodn6035@gmail.com>
2024-12-23 14:20:05 +0900

    spring mvc v2

--- src/main/java/hello/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/hello/springmvc/v1/SpringMemberListControllerV1.java
@@ -2,12 +2,11 @@ package hello.springmvc.v1;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-import java.util.Map;
 
 //@Controller
 @Controller
@@ -16,10 +15,10 @@ public class SpringMemberListControllerV1 {
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @RequestMapping("/springmvc/v1/members")
-    public ModelView process(Map<String, String> paramMap) {
+    public ModelAndView process() {
         List<Member> members = memberRepository.findAll();
-        ModelView mv = new ModelView("members");
-        mv.getModel().put("members", members);
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
         return mv;
     }
 }
--- src/main/java/hello/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/hello/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -8,11 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
-
 //@Controller
 @Controller
-public class SpringMemberFormControllerV1 {
+public class SpringMemberSaveControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
